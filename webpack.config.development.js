9e7eb298ec9fd0374919b18fdd7724076c01772c
Erick Bazán <bazan.erick@gmail.com>
2015-11-09 19:52:01 -0600

    d

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -23,7 +23,10 @@ module.exports = {
       include: path.join(__dirname, 'src')
     }, {
       test: /\.css$/,
-      loader: 'style!css',
+      loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1')
+    }, {
+      test: /\.scss$/,
+      loader: 'style!css?importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap'
     }, {
       test: /\.json$/,
       loaders: ['json'],
