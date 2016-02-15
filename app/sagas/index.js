494c778a057e41a8e851e17208b5aa0b9717269b
Erick Bazán <bazan.erick@gmail.com>
2016-02-15 00:39:17 -0600

    sagas 8

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -4,22 +4,26 @@
 
 /* eslint-disable no-constant-condition */
 
-import {take, put, call, fork} from 'redux-saga/effects';
+import {put, call, fork} from 'redux-saga/effects';
 import * as actions from '../actions';
+import {takeEvery} from 'redux-saga';
 import {api} from '../services';
 
 export function * getAllItems() {
-  while (yield take(actions.GET_ALL_ITEMS)) {
-    const items = yield call(api.getItems);
-    yield put(actions.receiveItems(items));
-  }
+  const items = yield call(api.getItems);
+  yield put(actions.receiveItems(items));
 }
 
-export function * startup() {
-  yield put(actions.getAllItems());
+export function * watchGetProducts(getState) {
+  /*
+    takeEvery will fork a new `checkout` task on each GET_ALL_PRODUCTS actions
+    i.e. concurrent GET_ALL_PRODUCTS actions are allowed
+  */
+  yield * takeEvery(actions.GET_ALL_ITEMS, getState);
 }
 
-export default function * root() {
-  yield fork(getAllItems);
-  yield fork(startup);
+export default function * root(getState) {
+  yield[fork(getAllItems),
+    fork(watchGetProducts, getState)
+  ];
 }
