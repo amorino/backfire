6423d5a768a816be5b5c7956b381080c312c5262
Erick Bazán <bazan.erick@gmail.com>
2016-02-21 14:31:46 -0600

    Fix Express Server

--- webpack/server.js
+++ webpack/server.js
@@ -1,26 +1,28 @@
+/*eslint-disable */
 var config = require('./webpack.dev.babel');
 var express = require('express');
 var webpack = require('webpack');
+var path = require('path');
 
 var app = express();
 var compiler = webpack(config);
 var server = 5000;
 
 app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
+  noInfo: true,
+  publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function (request, response){
-    response.redirect('/');
-})
+app.get('*', function response(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+});
 
 app.listen(server, 'localhost', function(err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('Listening at http://localhost:' + server);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at http://localhost:' + server);
 });
