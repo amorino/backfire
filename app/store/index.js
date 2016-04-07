6eadeedc04cc29073e4709aa992ad264206a29d7
Erick Bazán <bazan.erick@gmail.com>
2016-04-07 10:35:01 -0500

    Update Babel

--- app/store/index.js
+++ app/store/index.js
@@ -1,6 +1,3 @@
-/**
- * Create the store with asynchronously loaded reducers
- */
 import {createStore, applyMiddleware, compose} from 'redux';
 import {routerMiddleware} from 'react-router-redux';
 import sagaMiddleware from 'redux-saga';
@@ -9,10 +6,6 @@ import createReducer from '../reducers';
 import sagaMonitor from '../utils/sagaMonitor';
 
 export default function configureStore(initialState, history) {
-  // Create the store with two middlewares
-  // 1. sagaMiddleware: Imports all the asynchronous flows ("sagas") from the
-  //    sagas folder and triggers them
-  // 2. routerMiddleware: Syncs the location/URL path to the state
   const createStoreWithMiddleware = compose(
     applyMiddleware(routerMiddleware(history), sagaMiddleware(rootSaga), sagaMonitor),
     window.devToolsExtension ? window.devToolsExtension() : f => f
